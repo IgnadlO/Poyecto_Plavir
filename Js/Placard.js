5541bd5c93d7d6f7355eed5a9d140ebb210c4bb8
IgnadlO <57647369+IgnadlO@users.noreply.github.com>
2020-09-07 18:06:17 -0300

    7/9

--- Js/Placard.js
+++ Js/Placard.js
@@ -1,1 +1,10 @@
-Peron
+var nprenda = 0;
+
+function calcula_resta() {
+  let tprenda = ["Zapatillas","Remera","Campera","Pantalon","Gorra","Lentes"];
+  nprenda = nprenda + 1;
+  document.getElementById("Btprenda").innerHTML = tprenda[nprenda];
+  if (nprenda >= 5) {
+  	nprenda = -1;
+  }
+}
