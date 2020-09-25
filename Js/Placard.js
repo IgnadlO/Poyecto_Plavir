493d8aa37da2c64c73a17fc1be4ac6d7f4129808
IgnadlO <57647369+IgnadlO@users.noreply.github.com>
2020-09-24 23:27:40 -0300

    Carga 24/9

--- Js/Placard.js
+++ Js/Placard.js
@@ -1,23 +1,23 @@
 var tprenda = 0;
+var prendasDuras = new Array();
 
-function seleccionarPrenda(){
+
+function buscarPrenda(){
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
     	var objeto = JSON.parse(xhttp.responseText);
     	tropa = document.getElementById('tropa').value;
+      tropa = tropa.toLowerCase();
     	colorp = document.getElementById('colorp').value;
+      colorp = colorp.toLowerCase();
     	ac = 0;
     	i = 0;
     	while(ac != 1 && i < objeto.prendas.length){
-    		if(objeto.prendas[i].tipo == tropa)
-    		{
-    		if(objeto.prendas[i].colorp == colorp)
+    		if(objeto.prendas[i].tipo == tropa && objeto.prendas[i].colorp == colorp)
     		{
     			ac = 1;
     			document.getElementById("imagenSeleccionada").src= objeto.prendas[i].direccion;
-    		} else
-    			i ++;
     		} else 
     			i ++;
     	}
@@ -28,6 +28,119 @@ xhttp.onreadystatechange = function() {
 xhttp.open("GET", "Img/objeto.json", true);
 xhttp.send();
 }
+
+function filtroDuro(){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+      var objeto = JSON.parse(xhttp.responseText);
+      var x = 0;
+      tropa = document.getElementById('tropa').value;
+      tmoda = document.getElementById('moda').checked;
+      ttemporada = document.getElementById('temporada').value;
+      tevento = document.getElementById('evento').value;
+      prendasDuras = [];
+      for(var i = 0; i < objeto.prendas.length; i++){
+        if(objeto.prendas[i].moda == tmoda 
+          && (objeto.prendas[i].temporada == ttemporada || objeto.prendas[i].temporada == 5
+          || (ttemporada <= 2 && objeto.prendas[i].temporada == 6 ) || (ttemporada >= 3 && objeto.prendas[i].temporada == 7 ))
+          && (objeto.prendas[i].evento == tevento || objeto.prendas[i].evento== 5)
+          || (tevento <= 2 && objeto.prendas[i].evento == 6 ) || (tevento >= 2 && objeto.prendas[i].evento == 7 ))
+        {
+          prendasDuras[x] = i;
+          x++;
+        } 
+      }
+      elegirConjunto();
+}
+};
+xhttp.open("GET", "Img/objeto.json", true);
+xhttp.send();
+}
+
+function elegirConjunto(){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+      var objeto = JSON.parse(xhttp.responseText);
+      ttemperatura = document.getElementById('temperatura').value;
+      let mprenda;
+      var arrayPrendas = new Array();
+      var j = 0, limitePorEstacion;
+      var conjunto = new Array();
+
+      if (ttemporada == 1) 
+      {
+        limitePorEstacion = 2;
+        mprenda = ["zapatillas","remera","pantalon"];
+      }
+      else if (ttemporada == 2)
+      {
+        limitePorEstacion = 3;
+        mprenda = ["zapatillas","remera","campera","pantalon"];
+      }
+      else
+      {
+        if(ttemperatura <= 15){
+        limitePorEstacion = 4;
+        mprenda = ["zapatillas","remera","buzo","abrigo","pantalon"];
+      }else
+      limitePorEstacion = 3;
+        mprenda = ["zapatillas","remera","buzo","pantalon"];
+      }
+
+     for (var i = 0; i <= limitePorEstacion; i++) {
+      arrayPrendas = [];
+        for (var x = 0; x <= prendasDuras.length - 1; x++) {
+          np = prendasDuras[x];
+          if (objeto.prendas[np].tipo == mprenda[i]) {
+            arrayPrendas[j] = prendasDuras[x];
+            j ++;
+          }
+        }
+        prendaAleatorio = aleatorio(arrayPrendas);
+        subirConjunto(prendaAleatorio, mprenda[i]);
+        conjunto[i] = prendaAleatorio;
+        j = 0;
+     }
+    }
+  };
+xhttp.open("GET", "Img/objeto.json", true);
+xhttp.send();
+}
+
+
+function filtroBlando(){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+      var objeto = JSON.parse(xhttp.responseText);
+      for (var i = prendasDuras.length - 1; i >= 0; i--) {
+
+      }
+    }
+  };
+xhttp.open("GET", "Img/objeto.json", true);
+xhttp.send();
+}
+
+function subirConjunto(prenda, mprenda){
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200) {
+      var objeto = JSON.parse(xhttp.responseText);
+      if(mprenda == "buzo" || mprenda == "campera" || mprenda == "abrigo")
+        mprenda = "superior";
+      if(prenda != null)
+        document.getElementById("imagen"+ mprenda).src = objeto.prendas[prenda].direccion;
+      else
+        document.getElementById("imagen"+ mprenda).src = "";
+    }
+  };
+xhttp.open("GET", "Img/objeto.json", true);
+xhttp.send();
+}
+
 
 function borrarImagen(){
 	document.getElementById("imagenSeleccionada").src="";
@@ -63,3 +176,10 @@ function CambiarTipoRopa() {
   	tprenda = -1;
   }
   }
+
+  function aleatorio(variable){
+var aleatorio;
+aleatorio = variable[Math.floor(Math.random() * variable.length)];
+return aleatorio;
+}
+
--- SubirPrendas.php
+++ SubirPrendas.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "usuario";
+$database = "plavir";
 
 $enlace = mysqli_connect($servername, $username,$password, $database);
 
@@ -10,7 +10,7 @@ if ($enlace == false ) {
   die("Fallo la conexion a la base de datos" . $enlace->connect_error);
 }
 if(isset($_POST["submit"])){
-    
+    echo "Molto Bene.";
 }
 else{
     echo "No ha de Funcionar el servidor. Muchas Gracias e intentelo más tarde Gordo pelotudo.";
@@ -19,11 +19,14 @@ $nombre = $_POST['nombre'];
 $mail = $_POST['mail'];
 $contrasenia = $_POST['contrasenia'];
 $cumpleanios = $_POST['cumpleanios'];
-$placard = $_POST['placard'];
+$prendas = json_decode( $_POST["json"] );
+//var_dump($prendas->{"prendas"})
 
-$consulta = "INSERT INTO usuarios (nombre, mail, contrasenia, cumplenias, placard) VALUES (CURRENT_DATE(), '".$nombre ."' , '" .$mail . "','".$contrasenia ."','".$cumpleanios ."','".$placard ."' )";
+$consulta = "INSERT INTO plavir (nombre, mail, contraseña, ropa) VALUES ('".$nombre ."' , '" .$mail . "','".$contrasenia ."','".$prendas ."')";
 
-//$jason = '{ "prendas": [
+
+
+/*$jason = '{ "prendas": [
          {
             "direccion" : "Img/remera0.png",
             "tipo" : "remera",
@@ -33,6 +36,7 @@ $consulta = "INSERT INTO usuarios (nombre, mail, contrasenia, cumplenias, placar
             "temporada" : 1,
             "evento" : 1
         }]}'
+*/
 
 function getArraySQL($sql){
     $conexion = connectDB();
@@ -47,28 +51,35 @@ function getArraySQL($sql){
 
     while($row = mysqli_fetch_array($result))
     {
+    $direccion=$row['id'];
+    $tipo=$row['ropa'];
+    $colorp=$row['cp'];
+    $colors=$row['cs'];
+    $moda=$row['moda'];
+    $temporada=$row['temporada'];
+    $evento=$row['evento'];
+    $prenda[] = array('id'=> $id, 'tipo'=> $tipo, 'cp'=> $colorp, 'cs'=> $colors,
+                        'moda'=> $moda, 'temporada'=> $temporada, 'evento'=> $evento);
         $rawdata[$i] = $row;
         $i++;
     }
 
     disconnectDB($conexion); 
-
     return $rawdata; 
 }
 
-        $myArray = getArraySQL($sql);
-        echo json_encode($myArray);
+   //  $myArray = getArraySQL($sql);
+     //   echo json_encode($myArray);
 
 $resultado = mysqli_query($enlace,$consulta);
 mysqli_close($enlace);
 if($resultado){
-    echo '<script language="javascript">alert("mensaje enviado");document.location.href="index.html";
+    echo '<script language="javascript">alert("mensaje enviado");document.location.href="SubirPrendas.html";
           </script>';
 }
 else{
-    echo '<script language="javascript">alert("No se pudo enviar el mensaje");document.location.href="index.html";
+    echo '<script language="javascript">alert("No se pudo enviar el mensaje");document.location.href="SubirPrendas.html";
           </script>';
 }
-
 
 ?>
